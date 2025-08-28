16ede1dd1b3981770f101ef48a9010cb8085eb5f
Chitransh <chitranshgaur@outlook.com>
2025-08-28 13:59:27 +0530

    Template addition with urls and views

--- The_Blood_Community/The_Blood_Community/settings.py
+++ The_Blood_Community/The_Blood_Community/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'The_Blood_Community.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
