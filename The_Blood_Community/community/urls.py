2c5bbb5bfab2501b331e95ed10452e8be7ebd2eb
Chitransh <chitranshgaur@outlook.com>
2025-08-29 12:21:55 +0530

    views and urls built with link attachment in home.html

--- The_Blood_Community/community/urls.py
+++ The_Blood_Community/community/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('home/', views.Home.as_view(), name='home'),
     path('register/', views.Register.as_view(), name='register'),
     path('login/', views.Login.as_view(), name='login'),
-    path('needblood/')
+    path('needblood/', views.NeedBlood.as_view(), name='needblood'),
+    path('about/', views.About.as_view(), name='about'),
+    path('donateblood/', views.DonateBlood.as_view(), name='donateblood'),
+    path('communityandposts/', views.CommunityAndPosts.as_view(), name='communityandposts')
 ]
